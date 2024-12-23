e3c15f5f30e1164688fe92938892857f3d776068
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2024-12-23 19:52:32 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -53,11 +53,7 @@ def movie_quality(link):
     if response.status_code==200:
         tree = html.fromstring(response.content)
     for i in range(0,int(tree.xpath('count(//a[@class="skyblue-button"]/@href)'))):
-        try:
-            dicto[tree.xpath('//a[@class="skyblue-button"]//preceding-sibling::strong[2]/text()')[i]]=tree.xpath('//a[@class="skyblue-button"]/@href')[i]
-        except IndexError:
-            st.error("No results Found")
-            break
+        dicto[tree.xpath('//a[@class="skyblue-button"]//preceding-sibling::strong[2]/text()')[i]]=tree.xpath('//a[@class="skyblue-button"]/@href')[i]
     return dicto
 
 # Function: movie_torrent
